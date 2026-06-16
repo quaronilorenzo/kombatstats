a5e2f629dfd974824caa5b7d31412478d327c654
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2026-06-17 00:29:37 +0200

    Fixed creating entity user

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
@@ -16,5 +17,53 @@ public class User {
     @ElementCollection
     @Enumerated(EnumType.STRING)
     private List<Sport> sport;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Sport> getSport() {
+        return sport;
+    }
+
+    public void setSport(List<Sport> sport) {
+        this.sport = sport;
+    }
 }
 
