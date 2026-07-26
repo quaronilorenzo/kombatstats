8d11cb1e5ea5c56d373a9f0b7f88f6eef002ed54
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2026-07-26 22:08:19 +0200

    Added custom message for not valid exception

--- src/main/java/com/example/demo/utils/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/utils/GlobalExceptionHandler.java
@@ -2,10 +2,10 @@ package com.example.demo.utils;
 
 import com.example.demo.user.costants.UserErrors;
 import com.example.demo.user.exceptions.DuplicatedUserException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -14,6 +14,8 @@ import java.net.URI;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(DuplicatedUserException.class)
     public ResponseEntity<Object> handleUserDuplicatedException(DuplicatedUserException duplicatedUserException, WebRequest request){
         // RFC 9457 Problem Details for HTTP APIs
@@ -23,5 +25,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         problem.setTitle(UserErrors.userDuplicatedMessage);
         problem.setType(URI.create(UserErrors.userDuplicatedUri));
         return super.handleExceptionInternal(duplicatedUserException, problem, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, String.valueOf(ex.getFieldError()));
+        problemDetail.setTitle(String.valueOf(ex.getFieldError().getField() + " is mandatory"));
+        problemDetail.setType(URI.create("http://localhost:8080/argument-is-mandatory"));
+        return super.handleExceptionInternal(ex, problemDetail, headers, HttpStatus.BAD_REQUEST, request);
     }
 }
