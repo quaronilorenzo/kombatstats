4572fe287104cf098d1ecbe51d4f6b576b9985ed
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2026-09-19 15:08:08 +0200

    Added json type of response + response check

--- src/test/java/com/example/demo/testcontainers/JsonPathMock.java
+++ src/test/java/com/example/demo/testcontainers/JsonPathMock.java
@@ -1,5 +1,7 @@
 package com.example.demo.testcontainers;
 
 public interface JsonPathMock {
-    public static final String VALID_USER_JSON_PATH = "json/user/request/valid-user.json";
+    public static final String VALID_USER_REQUEST_JSON_PATH = "json/user/request/valid-user.json";
+    public static final String VALID_USER_RESPONSE_JSON_PATH = "json/user/response/created-user.json";
+
 }
--- src/test/java/com/example/demo/user/controller/UserControllerIntegrationTests.java
+++ src/test/java/com/example/demo/user/controller/UserControllerIntegrationTests.java
@@ -3,38 +3,34 @@ package com.example.demo.user.controller;
 
 import com.example.demo.testcontainers.AbstractIntegrationTest;
 import com.example.demo.testcontainers.JsonPathMock;
-import org.junit.experimental.results.ResultMatchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.util.StreamUtils;
-
-import java.nio.charset.Charset;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 public class UserControllerIntegrationTests extends AbstractIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
     @Test
     void shouldReturn201_whenaddUserCorrectly() throws Exception{
-        String json = readJson(JsonPathMock.VALID_USER_JSON_PATH);
+        String json = readJson(JsonPathMock.VALID_USER_REQUEST_JSON_PATH);
         RequestBuilder request = MockMvcRequestBuilders
                 .post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json)
                 .accept(MediaType.APPLICATION_JSON);
-        ResultMatcher expectedResponse = MockMvcResultMatchers.status().isCreated();
-        mockMvc.perform(request).andExpect(expectedResponse);
+        ResultMatcher expectedResponse = status().isCreated();
+        mockMvc.perform(request)
+                .andExpectAll(expectedResponse,
+                        content().json(readJson(JsonPathMock.VALID_USER_RESPONSE_JSON_PATH)),
+                        content().contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print());
     }
 
 }
